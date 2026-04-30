b43419d081df71b26af0377f5395f9802cf89802
manan-neural <mananvashi123@gmail.com>
2026-04-30 21:26:56 +0530

    Update Counting.java

--- Sorting/Counting.java
+++ Sorting/Counting.java
@@ -29,7 +29,7 @@ public class Counting {
         for(int i=0; i<arr.length; i++){
             System.out.print(arr[i]+" ");
         }
-        System.out.println();
+        System.out.println(" ");
     }
     
 
