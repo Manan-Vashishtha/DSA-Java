7b70401cef8169349e53ff20093be48d694ad19e
manan-neural <mananvashi123@gmail.com>
2026-05-18 23:59:33 +0530

    Update Palindrome.java

--- Strings/Palindrome.java
+++ Strings/Palindrome.java
@@ -13,7 +13,7 @@ public class Palindrome {
     }
 
     public static void main(String[] args) {
-        String str = "racecar";
+        String str = "malayalam";
         System.out.println(isPalindrome(str));
     }
 }
